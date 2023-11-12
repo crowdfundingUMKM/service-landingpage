ed2b2fb8a4ca6bcf3c0136ad92868a03948fafcf
ahhzaky <siahmadzaky@gmail.com>
2023-11-12 20:48:52 +0700

    Add pages faq

---
+++ src/pages/faq/index.jsx
@@ -0,0 +1,302 @@
+import FaqLayout from '@/pages/faq/layout';
+
+export default function Faq() {
+
+    return (
+        <FaqLayout>
+            <main>
+            <section className="section">
+                <div className="container">
+                    <div className="row justify-content-center mb-5">
+                    <div className="col-lg-6">
+                        <div className="section-title text-center">
+                        <p className="text-primary text-uppercase fw-bold mb-3">
+                            Pertanyaan Yang Sering Ditanyakan!
+                        </p>
+                        <h1>F.A.Q</h1>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="row justify-content-center">
+                    <div className="col-lg-9">
+                        <div className="accordion accordion-border-bottom" id="accordionFAQ">
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 active"
+                            id="heading-ebd23e34fd2ed58299b32c03c521feb0b02f19d9"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-ebd23e34fd2ed58299b32c03c521feb0b02f19d9"
+                            aria-expanded="true"
+                            aria-controls="collapse-ebd23e34fd2ed58299b32c03c521feb0b02f19d9"
+                            >
+                            Bisakah saya mendaftar hanya untuk melihat-lihat saja?
+                            </h2>
+                            <div
+                            id="collapse-ebd23e34fd2ed58299b32c03c521feb0b02f19d9"
+                            className="accordion-collapse collapse border-0 show"
+                            aria-labelledby="heading-ebd23e34fd2ed58299b32c03c521feb0b02f19d9"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Tentu saja! Crowdfunding UMKM terbuka untuk semua orang yang
+                                ingin mengeksplorasi dan mengembangkan bisnis. Anda dapat
+                                mendaftar untuk melihat-lihat proyek-proyek yang ada tanpa
+                                harus melakukan investasi. Jika Anda tertarik untuk lebih
+                                mendalami informasi atau memiliki pertanyaan, tim admin kami
+                                siap membantu memberikan penjelasan yang lebih lanjut agar
+                                Anda dapat memahami lebih banyak dan bersiap untuk membangun
+                                bisnis Anda sendiri di masa mendatang. Jangan ragu untuk
+                                menghubungi kami dan bergabunglah untuk mendukung pertumbuhan
+                                UMKM serta menemukan inspirasi bisnis yang menarik.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 "
+                            id="heading-a443e01b4db47b3f4a1267e10594576d52730ec1"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-a443e01b4db47b3f4a1267e10594576d52730ec1"
+                            aria-expanded="false"
+                            aria-controls="collapse-a443e01b4db47b3f4a1267e10594576d52730ec1"
+                            >
+                            Mengapa saya harus mendafatar di platform ini? apa yang membedakan
+                            dengan platform lainnya?
+                            </h2>
+                            <div
+                            id="collapse-a443e01b4db47b3f4a1267e10594576d52730ec1"
+                            className="accordion-collapse collapse border-0 "
+                            aria-labelledby="heading-a443e01b4db47b3f4a1267e10594576d52730ec1"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Anda harus mendaftar di platform ini karena Crowdfunding UMKM
+                                kami menawarkan beberapa keunggulan yang membedakan dengan
+                                platform lainnya. Pertama, kami telah terintegrasi dengan
+                                Midtrans, sehingga setiap pembayaran dapat langsung diteruskan
+                                ke platform kami, memastikan transaksi yang cepat dan aman.
+                                Kedua, proses pencairan modal dilakukan secara transparan
+                                setelah melalui proses peninjauan oleh pengguna, dan setiap
+                                dana dikelola dengan baik oleh tim Crowdfunding kami.
+                                </p>
+                                <p>
+                                Selain itu, kami memiliki tim konsultan ahli yang akan
+                                membantu Anda dalam tahapan pengajuan bisnis dan memberikan
+                                bimbingan terkait dengan pengembangan usaha Anda. Kami juga
+                                menawarkan kesempatan bagi investor untuk mendapatkan
+                                keuntungan dari investasi mereka setelah kebutuhan para UMKM
+                                terpenuhi atau modal yang diajukan telah tercapai.
+                                </p>
+                                <p>
+                                Selain itu, kami memiliki komitmen untuk memberikan keamanan
+                                dan kepercayaan kepada para pelaku usaha dan investor kami.
+                                Legalitas platform kami telah terjamin, dan data yang Anda
+                                berikan akan dikelola dengan aman dan terjamin privasinya.
+                                </p>
+                                <p>
+                                Kami berbeda dari platform lain karena kami fokus pada
+                                pertumbuhan dan kesuksesan UMKM dengan pendekatan yang
+                                transparan dan efektif. Bersama-sama, kita dapat memajukan
+                                sektor UMKM dengan kolaborasi yang kuat dan berkontribusi pada
+                                kemakmuran bisnis dan masyarakat luas.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 "
+                            id="heading-4b82be4be873c8ad699fa97049523ac86b67a8bd"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-4b82be4be873c8ad699fa97049523ac86b67a8bd"
+                            aria-expanded="false"
+                            aria-controls="collapse-4b82be4be873c8ad699fa97049523ac86b67a8bd"
+                            >
+                            Apakah saya sebagai user investor dapat langsung berdonasi ketika
+                            sudah mendaftar pada Crowdfunding?
+                            </h2>
+                            <div
+                            id="collapse-4b82be4be873c8ad699fa97049523ac86b67a8bd"
+                            className="accordion-collapse collapse border-0 "
+                            aria-labelledby="heading-4b82be4be873c8ad699fa97049523ac86b67a8bd"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Ya, sebagai user investor yang sudah mendaftar pada
+                                Crowdfunding UMKM, Anda dapat langsung berdonasi menggunakan
+                                layanan pembayaran yang telah terintegrasi dengan Midtrans.
+                                Namun, sebelum berdonasi, pastikan Anda telah memahami
+                                ketentuan dan syarat yang berlaku pada platform kami. Kami
+                                juga telah menyediakan panduan dan persyaratan yang harus
+                                dipenuhi sebelum berinvestasi, serta memastikan bahwa semua
+                                tawaran yang diberikan oleh pelaku usaha atau campaign telah
+                                melalui proses peninjauan dan memiliki legalitas yang
+                                terjamin.
+                                </p>
+                                <p>
+                                Kami selalu mengutamakan keamanan dan transparansi dalam
+                                setiap transaksi yang dilakukan di platform kami. Oleh karena
+                                itu, Anda juga diharapkan untuk menyediakan dokumen-dokumen
+                                yang diperlukan untuk memastikan identitas dan keabsahan
+                                donasi Anda. Dengan mengikuti prosedur yang telah kami
+                                tentukan, Anda dapat memberikan donasi dengan keyakinan bahwa
+                                dana Anda akan digunakan secara tepat dan membantu
+                                perkembangan UMKM yang berkualitas dan sesuai dengan aturan
+                                yang berlaku.
+                                </p>
+                                <p>
+                                Kami senantiasa berkomitmen untuk memberikan pengalaman
+                                terbaik bagi para user investor kami dan menghindari potensi
+                                tindakan penipuan atau kegiatan yang tidak sah di dalam
+                                platform kami. Dengan kerjasama dan dukungan dari seluruh
+                                pihak, kami dapat menciptakan lingkungan yang aman dan
+                                terpercaya bagi seluruh pelaku usaha dan investor dalam
+                                berdonasi di Crowdfunding UMKM.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 "
+                            id="heading-3e13e9676a9cd6a6f8bfbe6e1e9fc0881ef247b3"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-3e13e9676a9cd6a6f8bfbe6e1e9fc0881ef247b3"
+                            aria-expanded="false"
+                            aria-controls="collapse-3e13e9676a9cd6a6f8bfbe6e1e9fc0881ef247b3"
+                            >
+                            Bagaimana jika terjadinya pelanggran yang dilakukan oleh user?
+                            </h2>
+                            <div
+                            id="collapse-3e13e9676a9cd6a6f8bfbe6e1e9fc0881ef247b3"
+                            className="accordion-collapse collapse border-0 "
+                            aria-labelledby="heading-3e13e9676a9cd6a6f8bfbe6e1e9fc0881ef247b3"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Jika terjadi pelanggaran yang dilakukan oleh user di platform
+                                Crowdfunding UMKM, kami akan mengambil tindakan yang sesuai
+                                dengan peraturan dan kebijakan yang telah kami tetapkan.
+                                Setiap pelanggaran akan ditindaklanjuti dengan proses
+                                peninjauan dan investigasi secara teliti untuk memastikan
+                                keabsahan dan akurasi informasi.
+                                </p>
+                                <p>
+                                Jika terbukti adanya pelanggaran seperti penipuan, tindakan
+                                ilegal, atau aktivitas yang melanggar hukum, kami akan
+                                memberikan sanksi yang sesuai, termasuk pembatalan pencairan
+                                dana, pemblokiran akun, atau tindakan hukum lebih lanjut
+                                sesuai dengan peraturan yang berlaku.
+                                </p>
+                                <p>
+                                Kami selalu berupaya untuk menciptakan lingkungan yang aman
+                                dan terpercaya bagi seluruh pengguna platform kami. Oleh
+                                karena itu, kami mendorong semua pengguna untuk beroperasi
+                                dengan jujur, transparan, dan sesuai dengan peraturan yang
+                                berlaku agar dapat membangun ekosistem Crowdfunding UMKM yang
+                                berkelanjutan dan bermanfaat bagi semua pihak yang terlibat.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 "
+                            id="heading-0c2f829793a1f0562fea97120357dd2d43319164"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-0c2f829793a1f0562fea97120357dd2d43319164"
+                            aria-expanded="false"
+                            aria-controls="collapse-0c2f829793a1f0562fea97120357dd2d43319164"
+                            >
+                            Bisa tidak jika user ingin menarik uang atau pembatalan
+                            dikarenakan user batal dalam memberikan donasi?
+                            </h2>
+                            <div
+                            id="collapse-0c2f829793a1f0562fea97120357dd2d43319164"
+                            className="accordion-collapse collapse border-0 "
+                            aria-labelledby="heading-0c2f829793a1f0562fea97120357dd2d43319164"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Tentu saja, dalam sistem Crowdfunding UMKM, para investor
+                                memiliki fleksibilitas untuk menarik atau membatalkan donasi
+                                mereka sebelum campaign UMKM mencapai target atau tercapai
+                                100%. Ketika donasi belum tercapai targetnya, investor dapat
+                                melakukan pembatalan dan dana akan dikembalikan ke akun
+                                mereka. Hal ini memungkinkan para investor untuk merasa aman
+                                dan memiliki kendali penuh atas dana yang mereka investasikan.
+                                </p>
+                                <p>
+                                Namun, setelah campaign UMKM mencapai target atau tercapai
+                                100%, penarikan dana biasanya akan dilakukan setelah adanya
+                                kesepakatan dengan pihak pelaku usaha UMKM tentang penggunaan
+                                dana tersebut. Hal ini dilakukan untuk memastikan bahwa dana
+                                yang telah terkumpul digunakan dengan tepat sesuai dengan
+                                proposal bisnis yang diajukan oleh pelaku usaha UMKM.
+                                </p>
+                                <p>
+                                Kami memahami pentingnya transparansi dan keadilan dalam
+                                Crowdfunding UMKM. Oleh karena itu, kami berkomitmen untuk
+                                menyediakan informasi yang jelas dan akurat kepada para
+                                investor, sehingga mereka dapat membuat keputusan yang
+                                bijaksana dan mendukung pertumbuhan UMKM dengan keyakinan.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2
+                            className="accordion-header accordion-button h5 border-0 "
+                            id="heading-8fe6730e26db16f15763887c30a614caa075f518"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse-8fe6730e26db16f15763887c30a614caa075f518"
+                            aria-expanded="false"
+                            aria-controls="collapse-8fe6730e26db16f15763887c30a614caa075f518"
+                            >
+                            Jika anda ada pertanya lain anda dapat bertanya pada admin!
+                            </h2>
+                            <div
+                            id="collapse-8fe6730e26db16f15763887c30a614caa075f518"
+                            className="accordion-collapse collapse border-0 "
+                            aria-labelledby="heading-8fe6730e26db16f15763887c30a614caa075f518"
+                            data-bs-parent="#accordionFAQ"
+                            >
+                            <div className="accordion-body py-0 content">
+                                <p>
+                                Jika Anda memiliki pertanyaan lain atau butuh bantuan lebih
+                                lanjut, jangan ragu untuk bertanya kepada admin kami! Kami
+                                selalu siap membantu Anda dengan sepenuh hati dan memberikan
+                                informasi yang Anda perlukan untuk berpartisipasi dalam
+                                Crowdfunding UMKM. Terima kasih atas kepercayaan Anda kepada
+                                kami, dan kami berharap dapat menjadi bagian dari kesuksesan
+                                bisnis Anda di masa depan. Jangan ragu untuk menghubungi kami
+                                kapan pun Anda membutuhkan bantuan atau penjelasan lebih
+                                lanjut. Kami senang dapat mendukung pertumbuhan UMKM dan
+                                kesuksesan Anda sebagai investor di platform Crowdfunding UMKM
+                                kami.
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+
+            </main>
+        </FaqLayout>
+    );
+}
