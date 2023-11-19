02afb226bc772df248c6d6b629e19f65b53814b5
ahhzaky <siahmadzaky@gmail.com>
2023-11-19 22:51:33 +0700

    Update error 500

--- src/pages/500.jsx
+++ src/pages/500.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image'; // Jika Anda ingin menggunakan gambar
+import ErrorLayout from '@/pages/error/ErrorLayout'
 
 function Custom500() {
   return (
